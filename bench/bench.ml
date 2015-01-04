b52c3a36b3fa71b0a234dfb96259b5ca10e80c62
Sergey Fedorov <sergey.a.fedorov@gmail.com>
2015-01-04 08:34:19 +0800

    Merge bench

--- bench/bench.ml
+++ bench/bench.ml
@@ -9,22 +9,16 @@ let seed = 42
 
 let () =
   let thr l = print_endline ""; throughputN ~repeat:3 3 l |> tabulate in
-  thr [
+  let seed0, seed1 = (seed, seed * seed - 1) in
+  let () = thr [
     "hash", ignore $ H.hash, sample_string;
     "hash32", ignore $ H.hash32, sample_string;
     "hash64", ignore $ H.hash64, sample_string;
-  ];
-
-  thr [
     "hash_with_seed", ignore $ H.hash_with_seed ~seed, sample_string;
     "hash32_with_seed", ignore $ H.hash32_with_seed ~seed, sample_string;
     "hash64_with_seed", ignore $ H.hash64_with_seed ~seed, sample_string;
-  ];
-
-  let seed0, seed1 = (seed, seed * seed - 1) in
-  thr [
     "hash_with_seeds", ignore $ H.hash_with_seeds ~seed0 ~seed1, sample_string;
     "hash64_with_seeds", ignore $ H.hash64_with_seeds ~seed0 ~seed1, sample_string;
-  ];
-
+  ]
+  in
   ()
