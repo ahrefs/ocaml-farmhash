7d37b01017825f9e6e8a6b6e4895e0f53738ad5a
ip <igor@ahrefs.com>
2017-07-17 12:44:12 -0700

    avoid name clashes in C symbols

--- src/farmhash.ml
+++ src/farmhash.ml
@@ -1,19 +1,19 @@
 
-external hash : string -> int = "ml_hash"
-external hash32 : string -> int32 = "ml_hash32"
-external hash64 : string -> int64 = "ml_hash64"
+external hash : string -> int = "ml_farmhash_hash"
+external hash32 : string -> int32 = "ml_farmhash_hash32"
+external hash64 : string -> int64 = "ml_farmhash_hash64"
 
-external hash_with_seed : string -> seed:int -> int = "ml_hash_with_seed"
-external hash32_with_seed : string -> seed:int -> int32 = "ml_hash32_with_seed"
-external hash64_with_seed : string -> seed:int -> int64 = "ml_hash64_with_seed"
+external hash_with_seed : string -> seed:int -> int = "ml_farmhash_hash_with_seed"
+external hash32_with_seed : string -> seed:int -> int32 = "ml_farmhash_hash32_with_seed"
+external hash64_with_seed : string -> seed:int -> int64 = "ml_farmhash_hash64_with_seed"
 
-external hash_with_seeds : string -> seed0:int -> seed1:int -> int = "ml_hash_with_seeds"
-external hash64_with_seeds : string -> seed0:int -> seed1:int -> int64 = "ml_hash64_with_seeds"
+external hash_with_seeds : string -> seed0:int -> seed1:int -> int = "ml_farmhash_hash_with_seeds"
+external hash64_with_seeds : string -> seed0:int -> seed1:int -> int64 = "ml_farmhash_hash64_with_seeds"
 
-external hash128 : string -> (int64 * int64) = "ml_hash128"
-external hash128_with_seed : string -> seed:(int64 * int64) -> (int64 * int64) = "ml_hash128_with_seed"
+external hash128 : string -> (int64 * int64) = "ml_farmhash_hash128"
+external hash128_with_seed : string -> seed:(int64 * int64) -> (int64 * int64) = "ml_farmhash_hash128_with_seed"
 
-external fingerprint : string -> int = "ml_fingerprint"
-external fingerprint32 : string -> int32 = "ml_fingerprint32"
-external fingerprint64 : string -> int64 = "ml_fingerprint64"
-external fingerprint128 : string -> (int64 * int64) = "ml_fingerprint128"
+external fingerprint : string -> int = "ml_farmhash_fingerprint"
+external fingerprint32 : string -> int32 = "ml_farmhash_fingerprint32"
+external fingerprint64 : string -> int64 = "ml_farmhash_fingerprint64"
+external fingerprint128 : string -> (int64 * int64) = "ml_farmhash_fingerprint128"
--- src/farmhash_stubs.c
+++ src/farmhash_stubs.c
@@ -26,49 +26,49 @@ inline value create_tpl128(const uint128_t& hash128)
 
 extern "C" {
 
-CAMLprim value ml_hash(value data)
+CAMLprim value ml_farmhash_hash(value data)
 {
     CAMLparam1(data);
     uint64_t result = util::Hash64(String_val(data), caml_string_length(data));
     CAMLreturn(Val_int(result));
 }
 
-CAMLprim value ml_hash32(value data)
+CAMLprim value ml_farmhash_hash32(value data)
 {
     CAMLparam1(data);
     uint32_t result = util::Hash32(String_val(data), caml_string_length(data));
     CAMLreturn(caml_copy_int32(result));
 }
 
-CAMLprim value ml_hash64(value data)
+CAMLprim value ml_farmhash_hash64(value data)
 {
     CAMLparam1(data);
     uint64_t result = util::Hash64(String_val(data), caml_string_length(data));
     CAMLreturn(caml_copy_int64(result));
 }
 
-CAMLprim value ml_hash_with_seed(value data, value seed)
+CAMLprim value ml_farmhash_hash_with_seed(value data, value seed)
 {
     CAMLparam2(data, seed);
     uint64_t result = util::Hash64WithSeed(String_val(data), caml_string_length(data), Int_val(seed));
     CAMLreturn(Val_int(result));
 }
 
-CAMLprim value ml_hash32_with_seed(value data, value seed)
+CAMLprim value ml_farmhash_hash32_with_seed(value data, value seed)
 {
     CAMLparam2(data, seed);
     uint32_t result = util::Hash32WithSeed(String_val(data), caml_string_length(data), Int_val(seed));
     CAMLreturn(caml_copy_int32(result));
 }
 
-CAMLprim value ml_hash64_with_seed(value data, value seed)
+CAMLprim value ml_farmhash_hash64_with_seed(value data, value seed)
 {
     CAMLparam2(data, seed);
     uint64_t result = util::Hash64WithSeed(String_val(data), caml_string_length(data), Int_val(seed));
     CAMLreturn(caml_copy_int64(result));
 }
 
-CAMLprim value ml_hash_with_seeds(value data, value seed0, value seed1)
+CAMLprim value ml_farmhash_hash_with_seeds(value data, value seed0, value seed1)
 {
     CAMLparam3(data, seed0, seed1);
     uint64_t result = util::Hash64WithSeeds(
@@ -76,7 +76,7 @@ CAMLprim value ml_hash_with_seeds(value data, value seed0, value seed1)
     CAMLreturn(Val_int(result));
 }
 
-CAMLprim value ml_hash64_with_seeds(value data, value seed0, value seed1)
+CAMLprim value ml_farmhash_hash64_with_seeds(value data, value seed0, value seed1)
 {
     CAMLparam3(data, seed0, seed1);
     uint64_t result = util::Hash64WithSeeds(
@@ -84,14 +84,14 @@ CAMLprim value ml_hash64_with_seeds(value data, value seed0, value seed1)
     CAMLreturn(caml_copy_int64(result));
 }
 
-CAMLprim value ml_hash128(value data)
+CAMLprim value ml_farmhash_hash128(value data)
 {
     CAMLparam1(data);
     const uint128_t& hash128 = util::Hash128(String_val(data), caml_string_length(data));
     CAMLreturn(create_tpl128(hash128));
 }
 
-CAMLprim value ml_hash128_with_seed(value data, value seed_tpl)
+CAMLprim value ml_farmhash_hash128_with_seed(value data, value seed_tpl)
 {
     CAMLparam1(data);
     const uint128_t& seed = util::Uint128(Int64_val(Field(seed_tpl, 0)), Int64_val(Field(seed_tpl, 1)));
@@ -99,28 +99,28 @@ CAMLprim value ml_hash128_with_seed(value data, value seed_tpl)
     CAMLreturn(create_tpl128(hash128));
 }
 
-CAMLprim value ml_fingerprint(value data)
+CAMLprim value ml_farmhash_fingerprint(value data)
 {
     CAMLparam1(data);
     uint64_t result = util::Fingerprint64(String_val(data), caml_string_length(data));
     CAMLreturn(Val_int(result));
 }
 
-CAMLprim value ml_fingerprint32(value data)
+CAMLprim value ml_farmhash_fingerprint32(value data)
 {
     CAMLparam1(data);
     uint32_t result = util::Fingerprint32(String_val(data), caml_string_length(data));
     CAMLreturn(caml_copy_int32(result));
 }
 
-CAMLprim value ml_fingerprint64(value data)
+CAMLprim value ml_farmhash_fingerprint64(value data)
 {
     CAMLparam1(data);
     uint64_t result = util::Fingerprint64(String_val(data), caml_string_length(data));
     CAMLreturn(caml_copy_int64(result));
 }
 
-CAMLprim value ml_fingerprint128(value data)
+CAMLprim value ml_farmhash_fingerprint128(value data)
 {
     CAMLparam1(data);
     const uint128_t& hash128 = util::Fingerprint128(String_val(data), caml_string_length(data));
